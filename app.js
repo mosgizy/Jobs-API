595a6c8e512b9e7b0cb5b291c7f97acbf48136f9
mosgizy <moshood521@gmail.com>
2023-12-15 07:02:38 +0100

    welcome

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
+app.get('/', (req, res) => {
+  res.send('Jobs Api')
+})
+
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs',authMiddleware,jobsRouter)
 
